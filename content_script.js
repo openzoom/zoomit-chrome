c2343e2660aefe7800f50722d8e1e9e1df6ab9bc
aseemk <aseem.kishore@gmail.com>
2011-02-25 15:33:07 -0800

    Added context menu item for links!
    Right-clicking on link now shows "Open Link with Zoom.it".
    TODO: right-clicking on an *image* link shows both options, need to figure out
    how to fix this. It's confusing and it also combines both into a submenu.
    
    Also updated the manifest:
    + Improved description; now explicit about what this extension adds.
    + Upped the version from 1.0 to 1.1.
    + Fixed 3-space tabs to 4 spaces.
    
    Also minor code cleanup, and improved wording ("with" Zoom.it now).

--- content_script.js
+++ content_script.js
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright 2010 Daniel Gasienica <daniel@gasienica.ch>
+//  Copyright 2011 Aseem Kishore <aseem.kishore@gmail.com>
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -16,25 +17,42 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 
-var HOST = 'zoom.it';
+// Create context menu items for link, image and page clicks:
+// http://code.google.com/chrome/extensions/contextMenus.html#method-create
 
-// Create two context menu items for image and page clicks
-var image_menu = chrome.contextMenus.create({"title": "View Image on Zoom.it",
-                                             "contexts": ["image"],
-                                             "onclick": viewImageHandler});
-var image_menu = chrome.contextMenus.create({"title": "View Page on Zoom.it",
-                                             "contexts": ["page"],
-                                             "onclick": viewPageHandler});
+chrome.contextMenus.create({
+    "title": "Open Link with Zoom.it",
+    "contexts": ["link"],
+    "onclick": function (data, tab) {
+        //alert("Worked!");
+        //alert("data.linkUrl=" + data.linkUrl);
+        openWithZoomit(data.linkUrl);
+        //alert("Should be opening...");
+    }
+});
 
-function navigateToURL(url) {
-    chrome.tabs.create({url: url, selected: false});
+chrome.contextMenus.create({
+    "title": "View Image with Zoom.it",
+    "contexts": ["image"],
+    "onclick": function (data, tab) {
+        openWithZoomit(data.srcUrl);
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "View Page with Zoom.it",
+    "contexts": ["page"],
+    "onclick": function (data, tab) {
+        openWithZoomit(data.pageUrl);
+    }
+});
+
+function openWithZoomit(url) {
+    // http://code.google.com/chrome/extensions/tabs.html#method-create
+    chrome.tabs.create({
+        url: "http://zoom.it/?url=" + encodeURIComponent(url)
+    });
+    
+    // TEMP not sure what this is for? closes the extension's background page?
     window.close();
-}
-
-function viewImageHandler(info, tab) {
-    navigateToURL("http://" + HOST + "/?url=" + encodeURIComponent(info.srcUrl));
-}
-
-function viewPageHandler(info, tab) {
-    navigateToURL("http://" + HOST + "/?url=" + encodeURIComponent(info.pageUrl));
 }
